f9ca88e5368753f528d040d13526ff73d605eeef
MarietaVoskanyan <107118791+MarietaVoskanyan@users.noreply.github.com>
2022-06-22 14:13:55 +0400

    Create prog1.py

---
+++ prog1.py
@@ -0,0 +1,9 @@
+def name(anun):
+    return anun
+    
+def address(hasce):
+    return hasce
+    
+m = input("insert name: ")
+k = input("insert address: ")
+print(f'name is {name(m)} and address is {address(k)}')
